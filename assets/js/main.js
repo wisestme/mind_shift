c446404f7cec7514de581eca8abc27901a3e045e
wisestme01 <wyhzest@gmail.com>
2021-09-24 06:08:27 +0100

    toggle menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,20 @@
-// MENU SHOW Y HIDDEN
+// MENU SHOW/HIDE Y HIDDEN
 const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
+  navOpen = document.getElementById('nav-show'),
   navClose = document.getElementById('nav-close');
 
   // MENU SHOW
   // Validate if constant exists
-  if(navToggle)
-  {
-    navToggle.addEventListener('click', () => {
-      navMenu.classList.add('show-menu');
-  navToggle.classList.add('hide-icon');
-
-    })
-  }
-
-  // MENU HIDDEN
-  // Validate if constant exists
-if(navClose)
-{
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  navToggle.classList.remove('hide-icon');
-  });
-
+if(navOpen) {
+  navOpen.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
 }
 
-// Remove Menu
-const navLink = document.querySelectorAll('.nav__link');
-
-navLink.forEach(n => n.addEventListener('click', function (){
-  navMenu.classList.remove('show-menu');
-  navToggle.classList.remove('hide-icon');
-}))
+// MENU HIDE
+  // Validate if constant exists
+  if(navClose) {
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+    })
+  }
