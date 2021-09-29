bd444c6f4724075389df0101497d9aca5d63f70d
wisestme01 <wyhzest@gmail.com>
2021-09-29 06:56:42 +0100

    add slider control

---
+++ assets/js/slider.js
@@ -0,0 +1,9 @@
+$('.image_slider')
+	.not('slick-initialized')
+	.slick({
+	autoplay: true,
+	autoplaySpeed: 3000,
+	//dots: true,
+	//prevArrow: ".site-slider .slider-btn .prev",
+	//nextArrow: ".site-slider .slider-btn .next"
+});
