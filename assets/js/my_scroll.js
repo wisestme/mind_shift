0c1e3bff7f77671b01d9fdecab3d8de852764ce3
wisestme01 <wyhzest@gmail.com>
2021-11-06 08:51:02 +0100

    hide/show menu on scroll

---
+++ assets/js/my_scroll.js
@@ -0,0 +1,28 @@
+var ishiddenclass = 'hide',
+	b = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
+
+var ms = 0;
+
+document.onscroll = function(){
+	var t = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0),
+		a = t;
+
+	var elements = document.querySelectorAll('.large_nav');
+
+	elements.forEach(function(el) {
+		if(a < b) {
+			if (el.classList)
+			  el.classList.remove(ishiddenclass);
+			else
+			  el.className = el.className.replace(new RegExp('(^|\\b)' + ishiddenclass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+		if(a > b) {
+			if (el.classList)
+			  el.classList.add(ishiddenclass);
+			else
+			  el.className += ' ' + ishiddenclass;
+		}
+	});
+
+	b = a;
+};
